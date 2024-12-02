dcfcef8403304fc31dcc8feee09e2dd9f3c9b17b
HyeonProG <guswo3443@naver.com>
2024-12-02 22:07:22 +0900

    문자 출력, 각도 나누기

---
+++ src/level0/PCCENo1.java
@@ -0,0 +1,13 @@
+package level0;
+
+import java.util.Scanner;
+
+public class PCCENo1 {
+
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}

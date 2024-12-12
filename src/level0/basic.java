76167bce667d4bf323015759e47ddfaeab5bbbc0
HyeonProG <guswo3443@naver.com>
2024-12-13 00:13:50 +0900

    지폐 접기

--- src/level0/basic.java
+++ src/level0/basic.java
@@ -1,5 +1,14 @@
 package level0;
 
+import java.util.Scanner;
+
 public class basic {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+        sc.close();
+    }
 
 }

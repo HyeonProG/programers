084ab778299c083966adf0879b5310fbfe5a63c6
HyeonProG <guswo3443@naver.com>
2024-12-31 16:51:41 +0900

    배열 뒤집기

---
+++ src/level0/reverseList.java
@@ -0,0 +1,22 @@
+package level0;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class reverseList {
+
+    public int[] solution(int[] num_list) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : num_list) {
+            list.add(num);
+        }
+        Collections.reverse(list);
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+}

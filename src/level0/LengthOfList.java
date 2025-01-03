f818b790a6c75e48a44e5610762586d5f713bc88
HyeonProG <guswo3443@naver.com>
2025-01-03 15:39:58 +0900

    배열 원소의 길이

---
+++ src/level0/LengthOfList.java
@@ -0,0 +1,13 @@
+package level0;
+
+public class LengthOfList {
+
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] += strlist[i].length();
+        }
+        return answer;
+    }
+
+}

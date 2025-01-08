d5b40ce1c73585c6505388dedd4cc119a882ecfc
HyeonProG <guswo3443@naver.com>
2025-01-08 13:55:10 +0900

    제곱수 판별하기

---
+++ src/level0/DiscriminationOfSquareNumbers.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class DiscriminationOfSquareNumbers {
+
+    public int solution(int n) {
+        for (int i = 1; i * i <= n; i++) {
+            if (i * i == n) {
+                return 1;
+            }
+        }
+        return 2;
+    }
+
+}

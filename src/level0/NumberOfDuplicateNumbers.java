2bd541a0a3de3135457213ff4ac124061ae2ca08
HyeonProG <guswo3443@naver.com>
2025-01-16 14:06:03 +0900

    중복된 숫자의 개수

---
+++ src/level0/NumberOfDuplicateNumbers.java
@@ -0,0 +1,15 @@
+package level0;
+
+public class NumberOfDuplicateNumbers {
+
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) {
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+
+}

2060de18bdc3e7bd9943f7ae03225308e3ac12ed
HyeonProG <guswo3443@naver.com>
2025-01-16 14:18:52 +0900

    짝수와 홀수

---
+++ src/level1/EvenAndOdd.java
@@ -0,0 +1,15 @@
+package level1;
+
+public class EvenAndOdd {
+
+    public String solution(int num) {
+        String answer = "";
+        if (num % 2 == 0) {
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+
+}

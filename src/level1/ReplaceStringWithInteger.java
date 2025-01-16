a13a6ee9fda262b1c6a493d8352776d1c3d2e53b
HyeonProG <guswo3443@naver.com>
2025-01-16 14:12:26 +0900

    문자열을 정수로 바꾸기

---
+++ src/level1/ReplaceStringWithInteger.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class ReplaceStringWithInteger {
+
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}

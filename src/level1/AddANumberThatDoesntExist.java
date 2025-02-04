9455a8cae1708364c1dc8ff182314521b3ac789e
HyeonProG <guswo3443@naver.com>
2025-02-04 12:37:59 +0900

    없는 숫자 더하기

---
+++ src/level1/AddANumberThatDoesntExist.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class AddANumberThatDoesntExist {
+
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            answer += numbers[i];
+        }
+        return 45 - answer;
+    }
+
+}

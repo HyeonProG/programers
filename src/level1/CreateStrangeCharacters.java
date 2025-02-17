19127d3f0a5240781218289ce2e367fa2aca26f3
HyeonProG <guswo3443@naver.com>
2025-02-17 15:07:19 +0900

    이상한 문자 만들기

---
+++ src/level1/CreateStrangeCharacters.java
@@ -0,0 +1,25 @@
+package level1;
+
+public class CreateStrangeCharacters {
+
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                answer.append(c);
+                index = 0;
+            } else {
+                if (index % 2 == 0) {
+                    answer.append(Character.toUpperCase(c));
+                } else {
+                    answer.append(Character.toLowerCase(c));
+                }
+                index++;
+            }
+        }
+        return answer.toString();
+    }
+
+}
